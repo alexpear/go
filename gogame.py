ea596d4d88a00c12ea57a2ca978aa050ea12a3fc
alexpear <alexmpear@gmail.com>
2015-09-02 14:53:23 -0400

    Add some skeletons in temporarily

--- gogame.py
+++ gogame.py
@@ -7,14 +7,19 @@ class GoGame(app.App):
   def drawGrid(self, params=[]):
     print 'pretend this sentence is a gameboard.'
 
+  def put(self, params=[]):
+    pass
+
   def makeInputMap(self):
     return {
-      'ls': self.drawGrid
+      'ls': self.drawGrid,
+      'put': self.put
     }
 
   def __init__(self):
     app.App.__init__(self)
     self.inputMap.update(self.makeInputMap())
+    # self.gamestate = gamestate.Gamestate()
 
 # for testing
 game = GoGame()
